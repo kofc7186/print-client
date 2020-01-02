373162c9ea0371f9f4ce43250f29fb4af0dc4a95
Bob Callaway <bob.callaway@gmail.com>
2020-01-02 08:14:14 -0500

    use image with printer role

--- print-client.bat
+++ print-client.bat
@@ -1,14 +1,23 @@
 @ECHO OFF
 TITLE Knights of Columbus 7186 Print Client
-:: Check for GCP_PROJECT environment variable
+REM Check for GCP_PROJECT environment variable
 IF "%GCP_PROJECT%"=="" ECHO GCP_PROJECT environment variable not set! && exit /b 1
-:: Fail if creds.json does not exist in CWD
+REM Fail if creds.json does not exist in CWD
 IF NOT EXIST "creds.json" ECHO creds.json must exist in %CWD% and have the service account credentials to connect to the GCP account! && exit /b 1
-:: test for docker in path
-:: docker pull latest image
+REM test for docker in path
+REM ensure print spooler is not running on host (needs to run within container)
+powershell -cmd Stop-Service spooler
+powershell -cmd Set-Service spooler -StartupType Disabled
+REM docker pull latest image
 docker pull kofc7186/print-client:latest
-:: stop image if running
-docker stop print-client
-docker rm print-client
-:: run image, passing in cmd line args from this script
-docker run --name=print-client -v .:C:/data/ --restart on-failure -e GCP_PROJECT=%GCP_PROJECT% -d kofc7186/print-client %*
+REM stop image if running
+docker stop print-client-container || @echo on
+docker rm print-client-container || @echo on
+REM run image, passing in cmd line args from this script
+docker run --name=print-client-container ^                %= name the container so we can refer to it later =%
+--isolation=process ^                                     %= use process isolation =%
+-v .:C:\data\ ^                                           %= map CWD into container so we can pass GCP creds =%
+--restart on-failure ^                                    %= restart the container in the case where python crashes =%
+-e GCP_PROJECT='%GCP_PROJECT%' ^                          %= pass in the GCP project =%
+-e GOOGLE_APPLICATION_CREDENTIALS='C:/data/creds.json' ^  %= pass in the GCP creds =%
+kofc7186/print-client:latest %*                           %= always run the latest container =%
